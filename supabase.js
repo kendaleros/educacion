93603abd8bd43aaf1936a11c538e5fffbd93b803
kendaleros <114353271+kendaleros@users.noreply.github.com>
2026-06-14 18:04:48 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // Supabase Configuration
 // IMPORTANTE: Reemplaza estos valores con los tuyos de Supabase
 const SUPABASE_URL = 'https://fqcfbtjtilipyykaprxv.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxY2ZidGp0aWxpcHl5a2Fwcnh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5MjI4NjQsImV4cCI6MjA2NTQ5ODg2NH0.eyJpc3MiOiJzdXBhYmFzZSJ9';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxY2ZidGp0aWxpcHl5a2Fwcnh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0NjA4NTcsImV4cCI6MjA5NzAzNjg1N30.KvPfBJF7hquXRsqQiTSFNxy-uendPE1is-ihZ0v2wQw';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
